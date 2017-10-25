92c7edf610af1e67c267564c34cd5b5d8d699478
fmdkdd <fmdkdd@gmail.com>
2017-10-25 17:08:39 +0200

    Try to restore the opened state in the TreeViewer
    Still a bit flaky, because matching on names is not accurate.

--- plugins/fr.inria.atlanmod.emfviews.ui/src/fr/inria/atlanmod/emfviews/ui/editors/EViewEditor.java
+++ plugins/fr.inria.atlanmod.emfviews.ui/src/fr/inria/atlanmod/emfviews/ui/editors/EViewEditor.java
@@ -1,16 +1,19 @@
 package fr.inria.atlanmod.emfviews.ui.editors;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
@@ -95,8 +98,7 @@ public class EViewEditor extends EditorPart {
         if (element instanceof Resource) {
           return ((Resource) element).getURI().toString();
         } else if (element instanceof EObject) {
-          EObject o = ((EObject) element);
-
+          EObject o = (EObject) element;
           String className = o.eClass().getName();
 
           // Use the "name" feature, if it exists and is not empty
@@ -143,18 +145,68 @@ public class EViewEditor extends EditorPart {
     treeViewer.getControl().addMouseListener(new MouseAdapter() {
       @Override
       public void mouseUp(MouseEvent event) {
-        // TODO: expand the tree to the state it was before reloading the resource
         if (event.button == 3) {
+
+          // Save the expanded state
+          Object[] expandedElements = treeViewer.getExpandedElements();
+
           Resource r = new EmfViewsFactory().createResource(uri);
           try {
             r.load(null);
           } catch (IOException ex) {
             ex.printStackTrace();
           }
+
           treeViewer.setInput(new Object[] { r });
+
+          // Restore the expanded state
+          // Find the equivalent object in the new resource, and expand it
+          for (Object o : expandedElements) {
+            // We don't care about Resource (they are at the top-level)
+            if (o instanceof EObject) {
+              // @Correctness: the object path is not quite accurate for this purpose
+              // some objects have no name, but still have children
+              String fqn = getEObjectPath((EObject) o);
+              // @Optimize: that's a dumb and slow way to do it
+              TreeIterator<EObject> it = r.getAllContents();
+              while (it.hasNext()) {
+                EObject ro = it.next();
+                if (fqn.equals(getEObjectPath(ro))) {
+                  treeViewer.setExpandedState(ro, true);
+
+                  // Expand only the first match
+                  break;
+                }
+              }
+            } else if (o instanceof Resource) {
+              treeViewer.setExpandedState(r, true);
+            }
+          }
+        } else if (event.button == 1) {
+          // Print the object path on click
+          ITreeSelection selection = treeViewer.getStructuredSelection();
+          Object o = selection.getFirstElement();
+          if (o instanceof EObject) {
+            System.out.println(getEObjectPath((EObject) o));
+          }
         }
       }
     });
+  }
+
+  protected static String getEObjectPath(EObject o) {
+    List<String> comps = new ArrayList<>();
+
+    while (o != null) {
+      EStructuralFeature nameFeature = o.eClass().getEStructuralFeature("name");
+      if (nameFeature != null) {
+        comps.add((String) o.eGet(nameFeature));
+      }
+      o = o.eContainer();
+    }
+
+    Collections.reverse(comps);
+    return String.join(".", comps);
   }
 
   @Override
