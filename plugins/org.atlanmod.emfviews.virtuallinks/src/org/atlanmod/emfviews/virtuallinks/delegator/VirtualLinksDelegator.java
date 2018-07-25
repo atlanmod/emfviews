278f3714e63a26f05b36c248adf9416ae063a99e
fmdkdd <fmdkdd@0xc0de.fr>
2018-07-25 15:53:12 +0200

    Allow registering virtual links delegates in plain Java programs
    As a fallback to the Eclipse extension mechanism.

--- plugins/org.atlanmod.emfviews.virtuallinks/src/org/atlanmod/emfviews/virtuallinks/delegator/VirtualLinksDelegator.java
+++ plugins/org.atlanmod.emfviews.virtuallinks/src/org/atlanmod/emfviews/virtuallinks/delegator/VirtualLinksDelegator.java
@@ -14,12 +14,15 @@
 package org.atlanmod.emfviews.virtuallinks.delegator;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.atlanmod.emfviews.virtuallinks.WeavingModel;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -28,6 +31,12 @@ public class VirtualLinksDelegator {
 
   static final String EXTENSION_POINT = "org.atlanmod.emfviews.virtuallinks.delegator";
 
+  static Map<String, IVirtualLinksDelegate> extensionsToDelegate = new HashMap<>();
+
+  public static void register(String fileExtension, IVirtualLinksDelegate delegate) {
+    extensionsToDelegate.put(fileExtension, delegate);
+  }
+
   IVirtualLinksDelegate delegate;
   URI matchingModelURI;
 
@@ -35,24 +44,33 @@ public class VirtualLinksDelegator {
     this.matchingModelURI = matchingModelURI;
     String extension = matchingModelURI.fileExtension();
 
-    // Find the virtual links delegate that is the default handler for
-    // the file extension
+    IExtensionRegistry registry = Platform.getExtensionRegistry();
 
-    IExtension[] handlers = Platform.getExtensionRegistry()
-        .getExtensionPoint(EXTENSION_POINT).getExtensions();
+    if (registry == null) {
+      // We are not running in an Eclipse platform, so use the map instead
+      delegate = extensionsToDelegate.get(extension);
+      if (delegate == null) {
+        throw new IllegalStateException(String.format("No registered virtual links delegator for extension %s", extension));
+      }
+    } else {
+      // Find the virtual links delegate that is the default handler for
+      // the file extension
 
-    // Get all handlers for this file extension.  Default handlers are picked first.
-    IConfigurationElement handler = Arrays.stream(handlers)
-        .flatMap(h -> Arrays.stream(h.getConfigurationElements()))
-        .filter(c -> extension.compareToIgnoreCase(c.getAttribute("fileExtension")) == 0)
-        .sorted((a, b) -> Boolean.parseBoolean(a.getAttribute("default")) ? -1 : 1)
-        .findFirst()
-        .orElseThrow(() -> new IllegalStateException(String.format("No registered virtual links delegator for extension %s", extension)));
+      IExtension[] handlers = registry.getExtensionPoint(EXTENSION_POINT).getExtensions();
 
-    try {
-      delegate = (IVirtualLinksDelegate) handler.createExecutableExtension("class");
-    } catch (CoreException e) {
-      throw new IllegalStateException(String.format("Cannot instantiate virtual links delegator"), e);
+      // Get all handlers for this file extension.  Default handlers are picked first.
+      IConfigurationElement handler = Arrays.stream(handlers)
+          .flatMap(h -> Arrays.stream(h.getConfigurationElements()))
+          .filter(c -> extension.compareToIgnoreCase(c.getAttribute("fileExtension")) == 0)
+          .sorted((a, b) -> Boolean.parseBoolean(a.getAttribute("default")) ? -1 : 1)
+          .findFirst()
+          .orElseThrow(() -> new IllegalStateException(String.format("No registered virtual links delegator for extension %s", extension)));
+
+      try {
+        delegate = (IVirtualLinksDelegate) handler.createExecutableExtension("class");
+      } catch (CoreException e) {
+        throw new IllegalStateException(String.format("Cannot instantiate virtual links delegator"), e);
+      }
     }
   }
 
